b8fd53a70ef3373f755bf0e191491c16db6859c6
KANWAR717 <kanwartarsem@gmail.com>
2025-06-24 22:20:53 +0530

    update

--- Backend/Frontend/src/home/Leftpart/Left.jsx
+++ Backend/Frontend/src/home/Leftpart/Left.jsx
@@ -2,24 +2,19 @@ import React from "react";
 import Search from "./Search.jsx";
 import Users from "./Users.jsx";
 import useConversation from "../../statemanage/useConversation.js";
-
 function Left() {
   const { selectedConversation } = useConversation(); 
   return (
     <>
       <div
-        className={`fixed inset-0 md:ml-12 md:w-1/4 bg-white text-white font-semibold border-r border-gray-700 shadow-xl h-screen transition-transform duration-300 z-30 ${
+        className={`fixed inset-0 md:ml-12 md:w-1/4 bg-white text-black border border-slate-300 h-screen transition-transform duration-300 ${
           selectedConversation ? "transform -translate-x-full md:translate-x-0" : ""
         }`}
       >
-        {/* Search Bar */}
-        <div className="px-4 py-3 border-b border-gray-700">
-          <Search />
-        </div>
-
-        {/* User List */}
-        <div className="flex-1 overflow-y-auto px-2 py-4 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-800">
-          <Users />
+        
+        <Search />
+        <Users />
+        <div className="flex-1 overflow-y-auto">
         </div>
       </div>
     </>
