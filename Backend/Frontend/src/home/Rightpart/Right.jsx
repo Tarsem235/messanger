34c78466dda641cb0a8363e79166d4a5c781d066
KANWAR717 <kanwartarsem@gmail.com>
2025-06-24 22:12:47 +0530

    update

--- Backend/Frontend/src/home/Rightpart/Right.jsx
+++ Backend/Frontend/src/home/Rightpart/Right.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'; 
+import React, { useEffect } from 'react';
 import Chatuser from './Chatuser';
 import Messages from './Messages';
 import Typesend from './Typesend.jsx'; 
@@ -7,19 +7,18 @@ import { useAuth } from "../../context/AuthProvider.jsx";
 
 function Right() {
   const { selectedConversation, setSelectedConversation } = useConversation();
-
   useEffect(() => {
     setSelectedConversation(null);
   }, [setSelectedConversation]);
 
   return (
-    <div className="w-screen md:w-full lg:w-full bg-black text-white border-l border-gray-700 flex flex-col">
+    <div className="w-screen md:w-[100%] lg:w-[100%] bg-white text-gray-950 border-slate-100 flex flex-col ">
       {!selectedConversation ? ( 
         <NoChat /> 
       ) : (
-        <div className="flex flex-col h-full px-4 pt-4">
+        <div className="flex flex-col h-full md:pl-95">
           <Chatuser />
-          <div className="flex-1 overflow-y-auto mt-2 mb-2 rounded-lg border border-gray-700 p-3 bg-gray-900" style={{ maxHeight: 'calc(80vh - 8vh)' }}>
+          <div className="flex-1 overflow-y-auto" style={{ maxHeight: 'calc(80vh - 8vh)' }}>
             <Messages />
           </div>
           <Typesend />
@@ -33,18 +32,13 @@ export default Right;
 
 const NoChat = () => {
   const { authUser } = useAuth();
+  console.log(authUser);
   return (
-    <div className="flex items-center justify-center h-full sm-hiddne lg-block">
-      <div className="text-center">
-        <img
-          className="h-24 mx-auto opacity-80"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0UG5LYXDx2X78rDkRhREP51_FuSpyJbNoWA&s"
-          alt="No chat"
-        />
-        <h1 className="font-semibold text-xl mt-4 text-gray-300">
-          Select a chat to start messaging
-        </h1>
+    <div className='flex items-center justify-center h-full sm-hiddne lg-block'>
+      <div className='text-center absolute top-50 right-90 '>
+        <img className='h-20 mx-auto' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0UG5LYXDx2X78rDkRhREP51_FuSpyJbNoWA&s' alt="No chat" />
+        <h1 className='font-semibold text-xl '>Select a chat to start messaging</h1>
       </div>
     </div>
   );
-};
+}
